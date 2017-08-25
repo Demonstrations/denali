2a5b672f240bac61fec63b7fd65a07c3c7e53f69
胡汉权 <hanquan.hhq@alibaba-inc.com>
2017-08-25 20:12:35 +0800

    增加4个模组，分别是webRTC，日志，提醒，财务等。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+const HEROES:Hero[] = [{id:2, name:'xxxx'}, {id:3, name:'yyyy'}];
+export class Hero{
+  id:number;
+  name:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  hero:Hero = {
+    id: 1,
+    name: 'wo shi shui'
+  };
+  heroes = HEROES;
 }
